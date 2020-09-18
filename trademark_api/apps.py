88156368e3abc713334ca53034294f554b3861aa
Jerome Carter <jeromecarter0722@gmail.com>
2020-09-17 21:32:17 -0400

    Created API App

---
+++ trademark_api/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TrademarkApiConfig(AppConfig):
+    name = 'trademark_api'
--- trademark_backend/settings.py
+++ trademark_backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'trademark_backend.apps.trademark_api',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
