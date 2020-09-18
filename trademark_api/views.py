0736e635d34734a6a48cd46705f28be7b83eb7c3
Jerome Carter <jeromecarter0722@gmail.com>
2020-09-18 03:51:18 -0400

    Included JWT Authentication

--- trademark_api/views.py
+++ trademark_api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
 # Create your views here.
+
+class ExampleView(APIView):
+    permission_classes = (IsAuthenticated,)
+    def get(self, request):
+        content = {'message': 'Authenticated users can see this.'}
+        return Response(content)
