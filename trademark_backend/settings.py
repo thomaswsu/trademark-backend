483764c5526d32ca7a33d957acb93cecefc044b9
Jerome Carter <jeromecarter0722@gmail.com>
2020-09-17 21:39:38 -0400

    Fixed settings.py

--- trademark_backend/settings.py
+++ trademark_backend/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'trademark_backend.apps.trademark_api',
+    'trademark_api.apps.TrademarkApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
