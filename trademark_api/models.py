882cf82de0ee8a70a361629b6fbc709b8a39735f
thomaswsu <thomaswsu@users.noreply.github.com>
2020-09-29 14:11:06 -0700

    Resolve merge conflict and OneToOne with User and Portfolio

--- trademark_api/models.py
+++ trademark_api/models.py
@@ -1,9 +1,51 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
 from django.db.models.fields import BooleanField, CharField, DecimalField, FloatField, IntegerField
 
-# Create your models here.
+class User(AbstractUser):
+    username = None
+    email = models.EmailField('email address', blank=False, unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+class Order(models.Model):
+    ACTION_TYPES = (
+        ('B', 'Buy'),
+        ('S', 'Sell'),
+    )
+    ORDER_TYPES = (
+        ('M', 'Market'),
+        ('L', 'Limit'),
+    )
+    TIF_TYPES = (
+        ('GFD', 'Good For Day'),
+        ('GTC', 'Good Till Cancelled'),
+        ('IOC', 'Immediate or Cancel'),
+        ('FOK', 'Fill or Kill'),
+    )
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    action_type = models.CharField(max_length=1, choices=ACTION_TYPES, default='M', blank=False, null=False)
+    order_type = models.CharField(max_length=1, choices=ORDER_TYPES, default='M', blank=False, null=False)
+    execution_price = models.DecimalField(decimal_places=2, max_digits=16, blank=False, null=False)
+    time_in_force = models.CharField(max_length=3, choices=TIF_TYPES, default='GFD', blank=False, null=False)
+    filled = models.BooleanField(default=False, blank=False, null=False)
+    filled_at = models.DateTimeField(default=None, blank=False, null=True)
+    cancelled = models.BooleanField(default=False, blank=False, null=False)
+
+    def updateOrderStatus(self):
+        # if some call to api:
+            # set filled to true
+            # create a new Position in the user's portfolio
+        return(self.filled)
+
+    def cancelOrder(self):
+        # call API to cancel
+        self.cancelled = True
+
 class Portfolio(models.Model):
     # This is like ___init___
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     username = CharField(max_length=30)
     currentBalance = DecimalField(max_digits=12, decimal_places=2)
     # stocks = {} # This dictionary is indexed by a ticker. The ticker points to an int signifying the amount of stocks that the portfolio contains
@@ -46,7 +88,7 @@ class Position(models.Model):
         return(self.amount)
 
 class Stock(models.Model):
-    position = models.OneToOneField(Position, on_delete=models.CASCADE,primary_key=True)
+    position = models.OneToOneField(Position, on_delete=models.CASCADE, primary_key=True)
     dividendYield = DecimalField(max_digits=4, decimal_places=2)
 
 class Option(models.Model):
