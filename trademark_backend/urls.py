27f17e4f50ea88c4e4b8d63d70163aab4486b615
Jerome Carter <jeromecarter0722@gmail.com>
2020-09-29 06:14:49 -0400

    Added Orders Model

--- trademark_backend/urls.py
+++ trademark_backend/urls.py
@@ -21,9 +21,11 @@ from trademark_api import views
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('example/', views.ExampleView.as_view(), name='example'),
     path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/auth/new_user', views.AnonymousUserView.as_view(), name='create_user'),
-    path('api/auth/user', views.UserView.as_view(), name='user')
+    path('api/auth/user', views.UserView.as_view(), name='user'),
+    path('api/orders', views.OrdersView.as_view(), name='orders'),
+    path('api/order', views.CreateOrderView.as_view(), name='create_order'),
+    path('api/order/<int:order_id>', views.OrderView.as_view(), name='order'),
 ]
