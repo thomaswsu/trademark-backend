4b1027247ecb5472feb7a51e38f8275f829da88a
Jerome Carter <jeromecarter0722@gmail.com>
2020-10-01 19:22:39 -0400

    Added mods to models

--- trademark_api/models.py
+++ trademark_api/models.py
@@ -8,6 +8,11 @@ class User(AbstractUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
+OPTION_TYPES = (
+    ('C', 'Call'),
+    ('P', 'Put'),
+)
+
 class Order(models.Model):
     ACTION_TYPES = (
         ('B', 'Buy'),
@@ -32,6 +37,19 @@ class Order(models.Model):
     filled_at = models.DateTimeField(default=None, blank=False, null=True)
     cancelled = models.BooleanField(default=False, blank=False, null=False)
 
+    is_option_order = models.BooleanField(default=False, blank=False, null=False)
+    option_type = models.CharField(max_length=1, choices=OPTION_TYPES, blank=False, null=True)
+    option_strike = models.IntegerField(blank=False, null=True)
+    option_num_contracts = models.IntegerField(blank=False, null=True)
+    option_expiration = models.DateField(blank=False, null=True)
+
+    def verifyOptionOrder() -> bool:
+        if self.is_option_order:
+            return (self.option_type != None) and
+                   (self.option_strike != None) and
+                   (self.option_num_contracts != None and self.option_num_contracts > 0) and
+                   (self.option_expiration != None)
+
     def updateOrderStatus(self):
         # if some call to api:
             # set filled to true
@@ -42,4 +60,65 @@ class Order(models.Model):
         # call API to cancel
         self.cancelled = True
 
+<<<<<<< Updated upstream
 # Create your models here.
+=======
+class Portfolio(models.Model):
+    # This is like ___init___
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    username = CharField(max_length=30)
+    currentBalance = DecimalField(max_digits=12, decimal_places=2)
+    # stocks = {} # This dictionary is indexed by a ticker. The ticker points to an int signifying the amount of stocks that the portfolio contains
+    securites = {} # This is a dictionary indexed by a ticker. The ticker points to a dictionary of stock or call or put. The stock key points to an integer. The call or put key the points to dictionary of strike prices. 
+    #Example {AAPL : {stock : 100, call : {100 : positon}, put : {100 : positon}}, IBM : {call : {1 : positon, put : {1 : positon}}}} 
+    
+    def changeUsername(self, newUsername: str) -> str:
+        self.username = newUsername
+        return(self.username)
+
+    def addBalance(self, amountToAdd: DecimalField) -> DecimalField:
+        self.currentBalance += amountToAdd
+        # Check if decimals are fine? 
+        return(self.currentBalance)
+    
+
+    def addOption(self, ticker: CharField, securityType: str, strikePrice: DecimalField, price: FloatField, contracts: IntegerField) -> None: # We might not need price. We need to revise security type
+        if ticker in self.securites.keys():
+            if securityType in self.securites[ticker].keys():
+                if strikePrice in self.securites[ticker][securityType].keys(): # We are now pointing at an options object
+                    self.securites[ticker][securityType][strikePrice].addContracts()  
+                else:
+                    self.securites[ticker][securityType][strikePrice] = Position() # TODO: fill this out later 
+            else:
+                self.securites[ticker][securityType] = {}
+                self.securites[ticker][securityType][strikePrice] = Position() # TODO: Fill this in 
+        else:
+            self.securites[ticker] = {}
+            self.securites[ticker][securityType] = {}
+            self.securites[ticker][securityType][strikePrice] = Position() # TODO: fill this in
+                
+
+class Position(models.Model):
+    ticker = models.CharField(max_length=5)
+    price = -1 # TODO: this needs to be hooked up to an API
+    amount = 0 
+
+    def addAmount(self, amountToAdd: IntegerField) -> int:
+        self.amount += amountToAdd
+        return(self.amount)
+
+class Stock(models.Model):
+    position = models.OneToOneField(Position, on_delete=models.CASCADE, primary_key=True)
+    dividendYield = DecimalField(max_digits=4, decimal_places=2)
+
+class Option(models.Model):
+    position = models.OneToOneField(Position, on_delete=models.CASCADE,primary_key=True)
+    option_type = models.CharField(max_length=1, choices=OPTION_TYPES, blank=False, null=False)
+    strikePrice = models.IntegerField(blank=False, null=False)
+    numberOfContracts = models.IntegerField(default=1, blank=False, null=False)
+    expiration = models.DateField(blank=False, null=False)
+
+    def getContractValue():
+        # Need to hook this up to API
+        pass
+>>>>>>> Stashed changes
